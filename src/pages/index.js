6e5b05eafaaad3ad9a45dc62933130e7da9243b0
Phong Do <mrtuanphong@gmail.com>
2020-12-10 22:40:04 +0700

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,18 +6,20 @@ import { Helmet } from "react-helmet";
 
 export default ({ data }) => {
   return (
-    <Layout>
+    <div>
       <Helmet>
         <meta charSet="utf-8" />
         <title>{data.site.siteMetadata.title}</title>
         <meta name="description" content={data.site.siteMetadata.description} />
         <meta name="keywords" content={data.site.siteMetadata.keywords} />
         <meta name="author" content={data.site.siteMetadata.author} />
-        <link rel="canonical" href="http://mysite.com/example" />
+        <link rel="canonical" href={data.site.siteMetadata.url} />
+        <meta property="og:title" content={data.site.siteMetadata.title} />
+        <meta property="og:description" content={data.site.siteMetadata.description} />
+        <meta property="og:image" content={data.site.siteMetadata.url + data.site.siteMetadata.image} />
       </Helmet>
       <h1 className="text-5xl">Welcome to {data.site.siteMetadata.name}</h1>
-      {/* <Tabletop /> */}
-    </Layout>
+    </div>
   )
 }
 
@@ -29,7 +31,9 @@ export const query = graphql`
         author,
         title,
         description,
-        keywords
+        keywords,
+        url,
+        image
       }
     }
   }
