76282fe96c22b103b4c5be3d75740b1a66e36913
Phong Do <mrtuanphong@gmail.com>
2020-12-10 08:04:31 -0800

    update: copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,12 @@ export default ({ data }) => {
         <meta property="og:description" content={data.site.siteMetadata.description} />
         <meta property="og:image" content={data.site.siteMetadata.url + data.site.siteMetadata.image} />
       </Helmet>
-      <h1 className="text-5xl">Welcome to {data.site.siteMetadata.name}</h1>
+      <h1 className="text-3xl">Welcome to {data.site.siteMetadata.name}</h1>
+      <p>
+        <a href="https://www.lazada.vn/h%C6%B0%C6%A1ng-hoa-t%E1%BB%B1-nhi%C3%AAn-vi%E1%BB%87t-123260225/" title="Hương Hoa Tự Nhiên Việt trên Lazada">
+          Truy cập gian hàng trên Lazada ngay!
+        </a>
+      </p>
     </div>
   )
 }
