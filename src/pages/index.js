672d21ee5ce70601fc1ca6bba09dec32c3c120fd
Phong Do <mrtuanphong@gmail.com>
2020-12-14 00:03:56 +0700

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { graphql } from 'gatsby';
-import Layout from "../components/layout";
-import Tabletop from "../components/Tabletop";
+// import Layout from "../components/layout";
+// import Tabletop from "../components/Tabletop";
 import { Helmet } from "react-helmet";
+import { Button } from '@ahaui/react';
 
 export default ({ data }) => {
   return (
-    <div>
+    <div className="min-h-screen bg-black text-white">
       <Helmet>
         <meta charSet="utf-8" />
         <title>{data.site.siteMetadata.title}</title>
@@ -18,12 +19,28 @@ export default ({ data }) => {
         <meta property="og:description" content={data.site.siteMetadata.description} />
         <meta property="og:image" content={data.site.siteMetadata.url + data.site.siteMetadata.image} />
       </Helmet>
-      <h1 className="text-3xl">Welcome to {data.site.siteMetadata.name}</h1>
-      <p>
-        <a href="https://www.lazada.vn/h%C6%B0%C6%A1ng-hoa-t%E1%BB%B1-nhi%C3%AAn-vi%E1%BB%87t-123260225/" title="Hương Hoa Tự Nhiên Việt trên Lazada">
-          Truy cập gian hàng trên Lazada ngay!
-        </a>
-      </p>
+      <div className="max-w-3xl mx-auto p-4">
+        <h1 className="text-3xl mb-5">
+          {data.site.siteMetadata.name}
+        </h1>
+        <div >
+          <a 
+            href="https://www.lazada.vn/shop/huong-hoa-mart/" 
+            title="Hương Hoa Tự Nhiên Việt trên Lazada"
+            target="_blank"
+            className="inline-grid grid-cols-3 gap-x-4">
+            Gian hàng trên Lazada
+          </a>
+
+          <a 
+            href="https://shopee.vn/thanhhoa2015" 
+            title="Hương Hoa Tự Nhiên Việt trên Lazada"
+            target="_blank"
+            className="inline-grid grid-cols-3 gap-x-4">
+            Gian hàng trên Shopee
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
